30189abc4f7ea1356bfdb13cde5a353665118bc8
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-01 03:23:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(phrase)
   new_array = phrase.split(" ")
+  second_array = []
   
   new_array.each do |word|
-    "#{word.reverse}"
-    return new_array.join
+    second_array << "#{word.reverse}"
   end
   
 end
